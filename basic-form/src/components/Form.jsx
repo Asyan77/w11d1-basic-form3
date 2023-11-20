ae220bdf3ecf10228a8494ddd66b780b10503472
Blkalkin <88650606+Blkalkin@users.noreply.github.com>
2023-11-20 15:23:51 -0800

    finish basic form

--- basic-form/src/components/Form.jsx
+++ basic-form/src/components/Form.jsx
@@ -13,6 +13,8 @@ function Form () {
 
     }); 
 
+    const [errors, setErrors] = useState([]);
+
     function validations() {
         const emailRegex = /\S+@\S+.\S+/;
         const phoneRegex = /^[0-9]{10}$/;
@@ -21,11 +23,11 @@ function Form () {
         if(userInfo.name.length === 0) {
             errors.push("name can not be empty")
         }
-        if(userInfo.email.length === 0 || !emailRegex.test(formData.email)) {
+        if(userInfo.email.length === 0 || !emailRegex.test(userInfo.email)) {
             
             errors.push("email can not be empty")
         }
-        if(userInfo.phoneNumber.length === 0 || !phoneRegex.test(formData.phoneNumber)) {
+        if(userInfo.phoneNumber.length === 0 || !phoneRegex.test(userInfo.phoneNumber)) {
             errors.push("phone number must be properly formatted")
         }
         if(userInfo.bio.length > 280) {
@@ -45,9 +47,26 @@ function Form () {
                 staff: "",
                 bio: "",
                 signUpNotification: false
-        
             })
          }
+        let errors = validations();
+        if(errors.length){
+            setErrors(errors);
+        }else{
+            setErrors([])
+            alert("login is good!")
+
+        }
+         console.log(userInfo)
+    }
+
+    const showErrors = ()=>{
+        if(!errors.length) return null;
+        return (
+            <ul>
+                {errors.map(error => <li>{error}</li>)}
+            </ul>
+        )
     }
 
     function handleChange(input) {
@@ -59,23 +78,56 @@ function Form () {
     }
 
     return (
+        <div>
+        {showErrors()}
         <form onSubmit={handleSubmit}>
             <label >Name:
                 <input type="text" value={userInfo.name} onChange={handleChange("name")}></input>
             </label>
             <label>Email:
-                <input></input>
+                <input type="text" value={userInfo.email} onChange={handleChange("email")}></input>
             </label>
             <label>Phone Number:
-                <input></input>
+                <input type="text" value={userInfo.phoneNumber} onChange={handleChange("phoneNumber")}></input>
             </label>
             <label>Phone Type
-
+            <select name="phoneType" onChange={handleChange("phoneType")}>
+                <option value={userInfo.phoneType} diabled> Choose your phone type..</option>
+                <option>Home</option>
+                <option>Work</option>
+                <option>Cell</option>
+            </select>
             </label>
-            <select></select>
-            <textarea></textarea>
+            <label>
+            Staff:
+            <label>
+            <input
+                type="radio"
+                name="staff"
+                value={userInfo.staff}
+                onChange={handleChange("staff")}
+            /> Instructor
+            </label>
+            <label>
+            <input
+                type="radio"
+                name="staff"
+                value={userInfo.staff}
+                onChange={handleChange("staff")}
+            /> Student
+            </label>
+        </label>
+            <label>
+            Bio
+            <textarea value={userInfo.bio} onChange={handleChange("bio")} ></textarea>
+            </label>
+            <label>
+            Sign Up for Email Notifications
+            </label>
+            <input type="checkbox" value={userInfo.signUpNotification} onChange={handleChange("signUpNotification")}></input>
             <button>Submit</button>
         </form>
+        </div>
     )
 }
 
